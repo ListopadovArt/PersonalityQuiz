cec1987ae513f0cedbd401f742e87571df208b6a
ART_List <artbdarm9@mail.ru>
2021-02-03 23:39:48 +0300

    added View Controllers and segues

---
+++ PersonalityQuiz/QuestionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QuestionViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Artem Listopadov on 2/3/21.
+//  Copyright © 2021 Artem Listopadov. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
