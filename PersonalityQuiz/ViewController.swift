a1526bf2eb9955cde8411bd6c1861d527ed88506
ART_List <artbdarm9@mail.ru>
2021-02-03 21:08:24 +0300

    first commit

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Artem Listopadov on 2/3/21.
+//  Copyright © 2021 Artem Listopadov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
